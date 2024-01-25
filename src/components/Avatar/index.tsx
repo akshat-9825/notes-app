ca0143f1c5c2ff18512315290055d72d92bac884
Akshat-Dwivedi <akshat10dwivedi@gmail.com>
2024-01-25 19:18:41 +0530

    finishing up mobile version of project

--- src/components/Avatar/index.tsx
+++ src/components/Avatar/index.tsx
@@ -1,3 +1,7 @@
+import { useSelector } from "react-redux";
+import classNames from "classnames";
+import { selectGroupData } from "../../features/GroupSection/groupSlice";
+
 import styles from "./avatar.module.scss";
 
 const Avatar = ({ name, color }: { name: string; color: string }) => {
@@ -5,8 +9,14 @@ const Avatar = ({ name, color }: { name: string; color: string }) => {
   const avatarName =
     nameArr.length > 1 ? nameArr[0][0] + nameArr[1][0] : nameArr[0][0];
 
+  const { selectedGroup } = useSelector(selectGroupData);
+
   return (
-    <div style={{ backgroundColor: color }} className={styles.avatar}>
+    <div
+      style={{ backgroundColor: color }}
+      className={classNames(styles.avatar, {
+        [styles.avatar_navbar_mobile]: selectedGroup !== 0,
+      })}>
       {avatarName}
     </div>
   );
